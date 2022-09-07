1f3f41aec60b2fc4f86d1cd2d329d78e6867ac96
nezero <necer.radoicic@gmail.com>
2022-09-07 15:46:28 +0200

    adding note from home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,26 @@
 import NoteForm from './components/NoteForm'
 import NavBar from './components/NavBar'
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  let navigate = useNavigate();
+
+  const saveNote = async (newNote) => {
+    const res = await fetch('/notes', {
+      method: 'POST',
+      headers: {
+          'Content-type': 'application/json',
+      },
+      body: JSON.stringify(newNote)
+    });
+
+    if(res.ok) navigate("/notes");
+  }
+
   return (
     <>
       <NavBar />
-      <NoteForm />
+      <NoteForm saveNote={saveNote} />
     </>
   )
 }
