3e12248b58fdd094d921022b707da4f2932013ac
nezero <necer.radoicic@gmail.com>
2022-09-08 13:53:02 +0200

    adding api for quotes on home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,25 @@
 import NoteForm from './components/NoteForm'
 import NavBar from './components/NavBar'
+import QuoteCard from './components/QuoteCard';
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { Typography } from '@mui/material';
 
 function Home() {
+  const [quote, setQuote] = useState({});
   let navigate = useNavigate();
+
+  useEffect(() => {
+    getQuote();
+  }, [])
+
+  const getQuote = async () => {
+    const res = await fetch("https://api.quotable.io/random");
+    if(res.ok) {
+      const data = await res.json();
+      setQuote({content: data.content, author: data.author});
+    }
+  }
 
   const saveNote = async (newNote) => {
     const res = await fetch('/notes', {
@@ -21,6 +37,7 @@ function Home() {
     <>
       <NavBar />
       <NoteForm saveNote={saveNote} />
+      <QuoteCard quote={quote} />
     </>
   )
 }
